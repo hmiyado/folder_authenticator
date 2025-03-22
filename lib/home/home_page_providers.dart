1881212d491a13db216f34b3a9dd7f27a370d6d6
miyado <10195648+hmiyado@users.noreply.github.com>
2025-03-22 17:15:33 +0900

    Remove unused import for TotpEntry model in home_page_providers.dart

--- lib/home/home_page_providers.dart
+++ lib/home/home_page_providers.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:totp_folder/models/folder.dart';
-import 'package:totp_folder/models/totp_entry.dart';
 import 'package:totp_folder/repositories/folder_repository.dart';
 import 'package:totp_folder/repositories/totp_entry_repository.dart';
 
