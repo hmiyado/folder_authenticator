62d04adc0753cdaf973835629eb0a8eb5d7cc00d
miyado <10195648+hmiyado@users.noreply.github.com>
2025-03-29 21:31:52 +0900

    refactor: remove unnecessary folderPathProvider invalidation in subfolders provider

--- lib/home/folder/subfolders_provider.dart
+++ lib/home/folder/subfolders_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:totp_folder/home/folder/folder_path_provider.dart';
 import 'package:totp_folder/models/folder.dart';
 import 'package:totp_folder/repositories/folder_repository.dart';
 
@@ -9,6 +8,5 @@ part 'subfolders_provider.g.dart';
 @riverpod
 Future<List<Folder>> subfolders(Ref ref, {required int parentId}) async {
   final folderRepository = ref.watch(folderRepositoryProvider);
-  ref.invalidate(folderPathProvider);
   return folderRepository.getFolders(parentId);
 }
