537e8421765e706cec3fc1e370f595db03933809
miyado <10195648+hmiyado@users.noreply.github.com>
2025-03-29 17:50:39 +0900

    refactor: remove unused folder path display logic from folder_view.dart

--- lib/home/folder/folder_view.dart
+++ lib/home/folder/folder_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:totp_folder/home/folder/folder_entries_provider.dart';
-import 'package:totp_folder/home/folder/folder_path_provider.dart';
 import 'package:totp_folder/home/totp_entry_card.dart';
 import 'package:totp_folder/models/folder.dart';
 
@@ -13,36 +12,6 @@ class FolderView extends ConsumerWidget {
     required this.folderId,
   });
   
-  Widget _buildFolderPathDisplay(List<Folder> folderPath) {
-    if (folderPath.isEmpty) {
-      return const Text('Root');
-    }
-    
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          for (int i = 0; i < folderPath.length; i++) ...[
-            if (i > 0) 
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4.0),
-                child: Icon(Icons.chevron_right, size: 16),
-              ),
-            Text(
-              folderPath[i].name,
-              style: TextStyle(
-                fontWeight: i == folderPath.length - 1 ? FontWeight.bold : FontWeight.normal,
-                color: i == folderPath.length - 1 
-                    ? Colors.black 
-                    : Colors.grey[700],
-              ),
-            ),
-          ],
-        ],
-      ),
-    );
-  }
-
   Widget _buildFolderPathText(List<Folder> folderPath) {
     if (folderPath.isEmpty) {
       return const Text(
@@ -65,28 +34,11 @@ class FolderView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final folderPathAsyncValue = ref.watch(folderPathProvider(folderId: folderId));
     final allFolderEntries = ref.watch(allFolderEntriesProvider(folderId));
     
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // Folder path navigation display
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: folderPathAsyncValue.when(
-            data: (folderPath) {
-              return _buildFolderPathDisplay(folderPath);
-            },
-            loading: () => const SizedBox(
-              height: 24,
-              width: 24,
-              child: CircularProgressIndicator(strokeWidth: 2),
-            ),
-            error: (_, __) => const Text('Error loading folder path'),
-          ),
-        ),
-        const Divider(),
         Expanded(
           child: ListView(
             children: allFolderEntries.map((folderEntries){
